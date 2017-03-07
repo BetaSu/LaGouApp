a755df5c2fe9aaddf156510a95118185b51e55e6
suChang <icourt@icourtdeMacBook-Pro-5.local>
2017-03-07 23:15:18 +0800

    继续完成gulp配置文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var $ = require('gulp-load-plugins');
+var $ = require('gulp-load-plugins')();
 var open = require('open');
 
 var app={
@@ -8,11 +8,15 @@ var app={
     prdPath:'dist/'
 }
 
+gulp.task('build',['image','js','less','lib','html','json']);
+
+
 //拷贝文件
 gulp.task('lib',function () {
     gulp.src('bower_components/**/*.js')
         .pipe(gulp.dest(app.devPath+'vendor'))
         .pipe(gulp.dest(app.prdPath+'vendor'))
+        .pipe($.connect.reload());
 })
 
 //拷贝html
@@ -20,10 +24,62 @@ gulp.task('html',function () {
     gulp.src('app.srcPath'+'**/*.html')
         .pipe(gulp.dest(app.devPath))
         .pipe(gulp.dest(app.prdPath))
+        .pipe($.connect.reload());
 })
 
 gulp.task('json',function () {
     gulp.src('app.srcPath'+'**/*.json')
         .pipe(gulp.dest(app.devPath+'data'))
         .pipe(gulp.dest(app.prdPath+'data'))
+        .pipe($.connect.reload());
 })
+
+gulp.task('less',function () {
+    gulp.src(app.srcPath+'sytle/index.less')
+        .pipe($.less())
+        .pipe(gulp.dest(app.devPath+'css'))
+        .pipe($.cssmin())
+        .pipe(gulp.dest(app.prdPath+'css'))
+        .pipe($.connect.reload());
+})
+
+gulp.task('js',function () {
+    gulp.src(app.srcPath+'script/**/*.js')
+        .pipe($.concat('index.js'))
+        .pipe(gulp.dest(app.devPath+'js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(app.prdPath+'js'))
+        .pipe($.connect.reload());
+})
+
+gulp.task('image',function () {
+    gulp.src(app.srcPath+'image/**/*')
+        .pipe($.imagemin())
+        .pipe(gulp.dest(app.prdPath+'image'))
+        .pipe($.connect.reload());
+})
+
+//清除任务
+gulp.task('clean',function () {
+    gulp.src([app.devPath,app.prdPath])
+        .pipe($.clean())
+        .pipe($.connect.reload());
+})
+
+gulp.task('serve',['build'],function () {
+    $.connect.server({
+        root:[app.devPath],
+        livereload: true,
+        port: 1234
+    });
+    open('http://localhost:1234');
+
+    gulp.watch(app.srcPath+'script/**/*.js',['js']);
+    gulp.watch('bower_components/**/*',['lib']);
+    gulp.watch(app.srcPath+'**/*.html',['html']);
+    gulp.watch(app.srcPath+'data/**/*.json',['json']);
+    gulp.watch(app.srcPath+'style/**/*.less',['less']);
+    gulp.watch(app.srcPath+'image/**/*',['image']);
+})
+
+gulp.task('default',['serve']);
